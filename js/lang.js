74b3bc57371275a4d877c3f6aa2fa535b796adb9
retrogamersp <retrogamersp@gmail.com>
2018-12-13 12:27:05 +0000

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,38 @@
+function English() {
+    $("#navbarDropdown").text("Language");
+    $(".Home").text("Home");
+    $(".about").text("About");
+    $(".courses").text("Courses");
+    $(".apply").text("Apply");
+    $(".contact").text("Contact");
+    $(".title").text("Hot Beans Web Development & Design")
+    $(".desc").text("We are a small business focused on web design & development, we have a small team of free lancers who have a lot of experience with different development languages. Find out more about our team")
+    $(".contactus").text("CONTACT US!")
+    $(".hire").text("We are currently hiring!    to find out more")
+    $(".applylink").text("Click Here")
+    $(".herelink").text("here")
+
+
+
+}
+    
+function French(){
+    $("#navbarDropdown").text("La langue");
+    $(".Home").text("Accueil");
+    $(".about").text("Sur");
+    $(".courses").text("Cours");
+    $(".apply").text("Appliquer");
+    $(".contact").text("Contact");
+    $(".title").text("Conception et développement de sites Web Hot Beans");
+    $(".desc").text("Nous sommes une petite entreprise spécialisée dans la conception et le développement de sites Web. Nous avons une petite équipe de lanceurs gratuits qui ont beaucoup d’expérience dans différentes langues de développement. En savoir plus sur notre équipe")
+    $(".contactus").text("Contactez nous")
+}
+
+function Japanese(){
+    $("#navbarDropdown").text("言語");
+    $(".Home").text("Home");
+    $(".about").text("About");
+    $(".courses").text("Courses");
+    $(".apply").text("Apply");
+    $(".contact").text("Contact");  
+}
